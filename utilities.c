b0e696f0414231a4e64703cdc1023d4f9cac42e1
Marcello <marcellorusso.x@gmail.com>
2023-12-01 18:55:38 +0100

    creazione file gestione controlli e help

---
+++ utilities.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include "utilities.h"
+
+
+/*
+
+	Stampa a video l'help del programma
+
+	@params:
+		char* program_name: Nome del programma
+
+	@return:
+		void
+
+*/
+
+void help(char* program_name) {
+
+    printf(
+            "\n > Usage: %s [%s %s] <value> [%s %s] <value> [%s %s] <value>",
+            program_name,
+            SD_ARG_ROWS, DD_ARG_ROWS,
+            SD_ARG_COLUMNS, DD_ARG_COLUMNS,
+            SD_ARG_THREADS, DD_ARG_THREADS
+    );
+
+    printf("\n\n      Mandatory arguments:");
+    printf("\n        %s  %-20s Number of rows of the matrix", SD_ARG_ROWS, DD_ARG_ROWS);
+    printf("\n        %s  %-20s Number of columns of the matrix", SD_ARG_COLUMNS, DD_ARG_COLUMNS);
+    printf("\n        %s  %-20s Number of threads to use", SD_ARG_THREADS, DD_ARG_THREADS);
+
+    printf("\n\n      Error codes:");
+    printf("\n        %d %-20s Invalid number of arguments", ERR_ARGC, "ERR_ARGC");
+    printf(
+            "\n        %d %-20s Mandatory argument [%s %s] not provided",
+            ERR_NO_ROWS, "ERR_NO_ROWS",
+            SD_ARG_ROWS, DD_ARG_ROWS
+    );
+    printf(
+            "\n        %d %-20s Mandatory argument [%s %s] not provided",
+            ERR_NO_COLUMNS, "ERR_NO_COLUMNS",
+            SD_ARG_COLUMNS, DD_ARG_COLUMNS
+    );
+    printf(
+            "\n        %d %-20s Mandatory argument [%s %s] not provided",
+            ERR_NO_THREADS, "ERR_NO_THREADS",
+            SD_ARG_THREADS, DD_ARG_THREADS
+    );
+    printf("\n        %d %-20s Invalid number of rows provided", ERR_INVLD_ROWS, "ERR_INVLD_ROWS");
+    printf("\n        %d %-20s Invalid number of columns provided", ERR_INVLD_COLUMNS, "ERR_INVLD_COLUMNS");
+    printf("\n        %d %-20s Invalid number of threads provided", ERR_INVLD_THREADS, "ERR_INVLD_THREADS");
+    printf("\n        %d %-20s Unable to allocate memory\n", ERR_MEMORY, "ERR_MEMORY");
+
+}
+
+
+
+/*
+
+	Verifica l'integrita' degli argomenti passati in ingresso al programma
+
+	@params:
+		int argc: Numero di argomenti passati in ingresso al programma
+		char* argv[]: Argomenti passati in ingresso al programma
+
+	@return:
+		int: Codice errore/successo
+
+*/
+
+int check_args_code(int argc, char** argv) {
+
+    if(argc == 2 && !strcmp(argv[1], DD_ARG_HELP))
+        return SCC_HELP;
+
+    if(argc == 7) {
+
+        if(strcmp(argv[1], SD_ARG_ROWS) && strcmp(argv[1], DD_ARG_ROWS))
+            return ERR_NO_ROWS;
+
+        if(strcmp(argv[3], SD_ARG_COLUMNS) && strcmp(argv[3], DD_ARG_COLUMNS))
+            return ERR_NO_COLUMNS;
+
+        if(strcmp(argv[5], SD_ARG_THREADS) && strcmp(argv[5], DD_ARG_THREADS))
+            return ERR_NO_THREADS;
+
+        int rows = atoi(argv[2]);
+        int columns = atoi(argv[4]);
+        int threads = atoi(argv[6]);
+
+        if(rows <= 0)
+            return ERR_INVLD_ROWS;
+
+        if(columns <= 0)
+            return ERR_INVLD_COLUMNS;
+
+        if(threads <= 0)
+            return ERR_INVLD_THREADS;
+
+        return SCC_ARGS;
+
+    }
+
+    return ERR_ARGC;
+
+}
+
+
+
+/*
+
+	Gestisce il codice di errore o successo restituito da check_args_code
+
+	@params:
+		int argc: Numero di argomenti passati in ingresso al programma
+		char* argv[]: Argomenti passati in ingresso al programma
+
+	@return:
+		int: Codice errore/successo
+
+*/
+
+int check_args(int argc, char** argv) {
+    switch(check_args_code(argc, argv)) {
+
+        case SCC_HELP:
+            help(argv[0]);
+            return SCC_HELP;
+
+        case ERR_ARGC:
+            printf("\n <!> ERROR: Invalid number of arguments! For additional info type %s.\n", DD_ARG_HELP);
+            return ERR_ARGC;
+
+        case ERR_NO_ROWS:
+            printf(
+                    "\n <!> ERROR: Expected [%s %s] argument! For additional info type %s.\n",
+                    SD_ARG_ROWS, DD_ARG_ROWS, DD_ARG_HELP
+            );
+            return ERR_NO_ROWS;
+
+        case ERR_NO_COLUMNS:
+            printf(
+                    "\n <!> ERROR: Expected [%s %s] argument! For additional info type %s.\n",
+                    SD_ARG_COLUMNS, DD_ARG_COLUMNS, DD_ARG_HELP
+            );
+            return ERR_NO_COLUMNS;
+
+        case ERR_NO_THREADS:
+            printf(
+                    "\n <!> ERROR: Expected [%s %s] argument! For additional info type %s.\n",
+                    SD_ARG_THREADS, DD_ARG_THREADS, DD_ARG_HELP
+            );
+            return ERR_NO_THREADS;
+
+        case ERR_INVLD_ROWS:
+            printf("\n <!> ERROR: Invalid value for argument [%s %s]! For additional info type %s.\n",
+                   SD_ARG_ROWS, DD_ARG_ROWS, DD_ARG_HELP
+            );
+            return ERR_INVLD_ROWS;
+
+        case ERR_INVLD_COLUMNS:
+            printf("\n <!> ERROR: Invalid value for argument [%s %s]! For additional info type %s.\n",
+                   SD_ARG_COLUMNS, DD_ARG_COLUMNS, DD_ARG_HELP
+            );
+            return ERR_INVLD_COLUMNS;
+
+        case ERR_INVLD_THREADS:
+            printf("\n <!> ERROR: Invalid value for argument [%s %s]! For additional info type %s.\n",
+                   SD_ARG_THREADS, DD_ARG_THREADS, DD_ARG_HELP
+            );
+            return ERR_INVLD_THREADS;
+
+    }
+}
---
+++ utilities.h
@@ -0,0 +1,29 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#define SD_ARG_ROWS         "-r"
+#define SD_ARG_COLUMNS      "-c"
+#define SD_ARG_THREADS      "-t"
+
+#define DD_ARG_ROWS         "--rows"
+#define DD_ARG_COLUMNS      "--columns"
+#define DD_ARG_THREADS      "--threads"
+#define DD_ARG_HELP         "--help"
+
+#define SCC_ARGS            200
+#define SCC_HELP            201
+
+#define ERR_ARGC            400
+#define ERR_NO_ROWS         401
+#define ERR_NO_COLUMNS      402
+#define ERR_NO_THREADS      403
+#define ERR_INVLD_ROWS      404
+#define ERR_INVLD_COLUMNS   405
+#define ERR_INVLD_THREADS   406
+#define ERR_MEMORY          407
+
+void help(char* program_name);
+int check_args_code(int argc, char** argv);
+int check_args(int argc, char** argv);
+
+#endif
