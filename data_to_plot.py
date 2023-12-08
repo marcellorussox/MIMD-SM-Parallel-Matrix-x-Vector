4b1c7e267d91c91eb8d2f238716ac4d3f57d9263
Marcello <marcellorusso.x@gmail.com>
2023-12-08 19:05:46 +0100

    aggiunta parte per altri grafici

--- data_to_plot.py
+++ data_to_plot.py
@@ -4,25 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def create_graphs(df, x_col, y_cols, num_threads_fixed=None, ninput_fixed=None, x_min=None, x_max=None):
-    """
-    Args:
-        df (pd.DataFrame): Il DataFrame contenente i dati.
-        x_col (str): Nome della colonna da utilizzare per le ascisse.
-        y_cols (list): Lista di nomi delle colonne da utilizzare per le ordinate.
-        num_threads_fixed (int): Numero di thread fisso (usato solo se specificato).
-        ninput_fixed (int): Numero di input fisso (usato solo se specificato).
-        save_path (str): Percorso per salvare i grafici (formato: 'nome_file.png').
-        x_min (float): Valore minimo per le ascisse.
-        x_max (float): Valore massimo per le ascisse.
-    """
+def create_graphs(df, x_col, y_cols, x_min=None, x_max=None):
 
     if x_min is not None and x_max is not None:
         df = df[(df[x_col] >= x_min) & (df[x_col] <= x_max)]
 
     grouped_data = df.groupby(['ROWS', 'COLUMNS'])
-
-    print(grouped_data)
 
     for (rows, columns), group in grouped_data:
         plt.figure()
@@ -48,6 +35,26 @@ def create_graphs(df, x_col, y_cols, num_threads_fixed=None, ninput_fixed=None, 
             plt.savefig(f'output/plots/plot_{y_col.lower()}_10_{rows_exp}x10_{columns_exp}.png')
             plt.close()
 
+
+#    for y_col in y_cols:
+#        plt.figure()
+#
+#        order = 0
+#
+#        for (rows, columns), group in grouped_data:
+#            rows_exp = int(math.log10(rows))
+#            columns_exp = int(math.log10(columns))
+#            plt.plot(group[x_col], group[y_col],
+#                     label=f'(Matrix 10^{rows_exp} x 10^{columns_exp}) x (Vector 10^{columns_exp})')
+#            order = rows_exp + columns_exp
+#
+#        plt.title(f'On the order of 10^{order}')
+#        plt.xlabel('Number of threads')
+#        plt.ylabel(y_col.capitalize())
+#        plt.legend()
+#        plt.savefig(f'output/plots/plot_{y_col.lower()}_e{order}.png')
+#        plt.close()
+#
 
 # Esempio di utilizzo con intervallo per le ascisse
 csv = pd.read_csv('data.csv')
